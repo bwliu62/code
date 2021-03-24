b820cb63286fbedfc9eb5c144c0a8d46aa490e5d
bowen <bwliu62@gmail.com>
2021-03-23 21:56:47 -0500

    update

--- src/recursion3/BinaryTreePathSumToTargetIII.java
+++ src/recursion3/BinaryTreePathSumToTargetIII.java
@@ -1,4 +1,5 @@
 package recursion3;
+import java.util.*;
 /**
  * Given a binary tree in which each node contains an integer number. Determine if there exists a path (the path can only be from one node to itself or to any of its descendants), the sum of the numbers on the path is the given target number.
  *
@@ -45,6 +46,7 @@ package recursion3;
  *     4
  * */
 public class BinaryTreePathSumToTargetIII {
+    // method 1 : best case O(nlogn)  worst case n^2 ; SC: O(height) -> On
     public boolean exist(TreeNode root, int target) {
         // Write your solution here
         if (root == null) {
@@ -57,5 +59,33 @@ public class BinaryTreePathSumToTargetIII {
             return false;
         }
         return root.key == target || helper(root.left, target - root.key) || helper(root.right, target- root.key);
+    }
+
+    // method 2: we consider the path as a linked list
+    public boolean exist2(TreeNode root, int target) {
+        if (root == null) {
+            return false;
+        }
+        Set<Integer> prefixSums = new HashSet<>();
+        prefixSums.add(0);
+        return helper2(root, prefixSums, 0 ,target);
+    }
+
+    private boolean helper2(TreeNode root, Set<Integer> prefixSums, int preSum, int target){
+        preSum += root.key;
+        if (prefixSums.contains(preSum - target)) {
+            return true;
+        }
+        boolean needRemove = prefixSums.add(preSum);
+        if (root.left != null && helper2(root.left, prefixSums, preSum, target)) {
+            return true;
+        }
+        if (root.right != null && helper2(root.right, prefixSums, preSum, target)) {
+            return true;
+        }
+        if (needRemove) {
+            prefixSums.remove(preSum);
+        }
+        return false;
     }
 }
