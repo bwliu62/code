f6dd9b254ab3aa172a34f4355f5aa6846a20e5ec
bowen <bwliu62@gmail.com>
2021-03-24 20:59:45 -0500

    update

--- src/recursion3/MaximumPathSumBinaryTreeIII.java
+++ src/recursion3/MaximumPathSumBinaryTreeIII.java
@@ -2,7 +2,7 @@ package recursion3;
 /**
  * Given a binary tree in which each node contains an integer number. Find the maximum possible subpath sum(both the starting and ending node of the subpath should be on the same path from root to one of the leaf nodes, and the subpath is allowed to contain only one node).
  *
- * Assumptions
+ * Assumptions:
  *
  * The root of given binary tree is not null
  * Examples
